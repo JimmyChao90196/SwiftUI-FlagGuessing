8afc072bcfad7bd7e7ce4f81551ebd12663791c7
Jimmy <myname90196@gmail.com>
2024-03-10 22:50:35 +0800

    -practice- 1. Learn button and images in SwiftUI

---
+++ FlagGuessing/FlagGuessing/FlagGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingApp.swift
+//  FlagGuessing
+//
+//  Created by JimmyChao on 2024/3/10.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
